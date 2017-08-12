d9af777a9187becc0f2995131539eca4f213e3e6
git996 <iamnewdev@gmail.com>
2017-08-12 17:17:05 +1000

    script2 changes

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
-console.log('Current Version is ' + process.version);
+
+document.write('Current Version is ' + process.version);
 var fs  =  require('fs');
 content = fs.readFileSync('./package.json', 'utf-8');
-content = JSON.parse(content)
-console.log(content.dependencies);
+alert(content);
